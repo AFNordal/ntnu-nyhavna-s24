3d0783651727b45b8d1a4b7f96c42e189c70ec12
Rasmus Anker Fossen Nordal <nordalrasmus01@gmail.com>
2024-06-14 14:01:41 +0000

    parsing

---
+++ tracking-box/sd-test/parsing/parse_IMU.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser
+import struct
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+def get_fname():
+    parser = ArgumentParser()
+    parser.add_argument("file")
+    args = parser.parse_args()
+    return args.file
+
+def main():
+    fname = get_fname()
+    with open(fname, "rb") as file:
+        contents = file.read()
+    data = np.array([i for i in tqdm(struct.iter_unpack("<"+"h"*6+"I"+"B"*4, contents))])
+    plt.plot(data[:, 6], data[:, 0])
+    plt.show()
+        # for sample in data:
+        #     ax, ay, az, gx, gy, gz, j, f, _, _, _ = sample
+
+
+
+
+if __name__ == "__main__":
+    main()
