78aee20bfa2894624270d2d98756762b9d2c3dd6
Rasmus Anker Fossen Nordal <nordalrasmus01@gmail.com>
2024-08-12 14:21:10 +0000

    removed and ignored build dir again

--- tracking-box/firmware/build/CMakeFiles/sd_test.dir/include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/sd_driver/SPI/crc.c.obj.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/sd_test.dir/include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/sd_driver/SPI/crc.c.obj: \
- /include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/sd_driver/SPI/crc.c \
- /include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/sd_driver/SPI/crc.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h
--- tracking-box/firmware/build/CMakeFiles/sd_test.dir/include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/src/crc.c.obj.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/sd_test.dir/include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/src/crc.c.obj: \
- /include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/src/crc.c \
- /include/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/include/crc.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h
